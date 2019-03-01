a765e2c1b5b1d316eace6d6a8be63b1d150d7167
gus-araujo <gus.araujo@enjoei.com.br>
2019-02-28 21:54:57 -0300

    add database creation

--- case/solucao.rb
+++ case/solucao.rb
@@ -2,12 +2,26 @@ if !`gem list`.include? 'pg'
     `gem install pg`
 end
 require 'pg'
+
+`psql -U postgres -c "DROP DATABASE solucao;"`
+`psql -U postgres -c "DROP USER solucao;"`
 `sudo -i -u postgres createuser solucao`
 `sudo -i -u postgres createdb solucao`
+
 `psql -U postgres solucao -c "ALTER DATABASE solucao OWNER TO solucao;"`
+
+current_time = 0
+max_time = 480
+afternoon_session = 180
+morning_session = 180
 
 conn = PG.connect(dbname: 'solucao', user: 'solucao')
 conn.exec("CREATE TABLE solucao(
+    id serial primary key,
+    talk VARCHAR,
+    duration INTEGER
+    )")
+conn.exec("CREATE TABLE exclusao(
     id serial primary key,
     talk VARCHAR,
     duration INTEGER
@@ -16,17 +30,16 @@ conn.exec("CREATE TABLE solucao(
 input = File.open("input.txt")
 input.each do |linha|
     if linha.include?"lightning"
-    linha = linha.delete('lightning')
+    key = linha[0..(linha.length - "lightning".length)]
         conn.exec("INSERT INTO solucao (talk, duration)
-            VALUES('#{linha}', 5)")
+            VALUES('#{key}', 5)")
     else 
         key = linha[0...(linha.length)-6]
         value = linha[(linha.length)-6...linha.length]
-        value = value.delete('min').to_i
-        puts value
-        #conn.exec("INSERT INTO solucao VALUES('#{key}', #{value})")
+        value = value.delete("min")
+        conn.exec("INSERT INTO solucao (talk, duration)
+        VALUES('#{key}', #{value})")
     end
-
 end
-
-talks.sort.values
+puts `psql -U solucao -c "select * from solucao"`
+conn.close
