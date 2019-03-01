af3655d792e8fb80c6c382b893e27cd0a24e9fb2
gus-araujo <gus.araujo@enjoei.com.br>
2019-02-28 21:02:24 -0300

    add pg installing and database creating script

--- case/solucao.rb
+++ case/solucao.rb
@@ -1,13 +1,30 @@
+if !`gem list`.include? 'pg'
+    `gem install pg`
+end
+require 'pg'
+`sudo -i -u postgres createuser solucao`
+`sudo -i -u postgres createdb solucao`
+`psql -U postgres solucao -c "ALTER DATABASE solucao OWNER TO solucao;"`
+
+conn = PG.connect(dbname: 'solucao', user: 'solucao')
+conn.exec("CREATE TABLE solucao(
+    id serial primary key,
+    talk VARCHAR,
+    duration INTEGER
+    )")
+
 input = File.open("input.txt")
-talks = {}
 input.each do |linha|
     if linha.include?"lightning"
-        linha.tr("lightning","")
-        talks.store(linha,"5min") 
+    linha = linha.delete('lightning')
+        conn.exec("INSERT INTO solucao (talk, duration)
+            VALUES('#{linha}', 5)")
     else 
         key = linha[0...(linha.length)-6]
         value = linha[(linha.length)-6...linha.length]
-        talks.store(key,value) 
+        value = value.delete('min').to_i
+        puts value
+        #conn.exec("INSERT INTO solucao VALUES('#{key}', #{value})")
     end
 
 end
