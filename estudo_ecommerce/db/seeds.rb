57a1e60be180fc9a2f605b932c6aa0cf9fe2d932
gus-araujo <monothreshvrau@gmail.com>
2019-04-17 23:21:31 -0300

    making user faker db

--- estudo_ecommerce/db/seeds.rb
+++ estudo_ecommerce/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 500.times do
+#     User.create(
+#         name: Faker::Name.name,
+#         age: Faker::Number.between(11,70),
+#         gender: Faker::Gender.binary_type
+#     )
+# end
