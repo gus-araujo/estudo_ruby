6e64e625911cc4c5914ff80f43611fed7fc4378e
gus-araujo <gus.araujo@enjoei.com.br>
2019-04-20 02:04:09 -0300

    add user authentication and image upload for the new products model

--- estudo_ecommerce/config/routes.rb
+++ estudo_ecommerce/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :products
+  get 'contato', to: 'home#contact'
+  get 'sobre', to: 'home#about'
+  devise_for :users
+  root 'home#Index'
+  
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- estudo_ecommerce/db/schema.rb
+++ estudo_ecommerce/db/schema.rb
@@ -10,7 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_202249) do
+ActiveRecord::Schema.define(version: 2019_04_20_045132) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.float "price"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +51,13 @@ ActiveRecord::Schema.define(version: 2019_04_17_202249) do
     t.string "gender"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
